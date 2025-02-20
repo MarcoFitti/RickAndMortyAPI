f4a1a5ff4f870eaa5faa908c19a416313c010d90
MarcoFitti <MarcoFitti@users.noreply.github.com>
2025-02-20 16:24:34 +0100

    2nd Commit: I can now call and visualize 1 character from the show.

--- app/src/main/java/com/example/rickmortyapi/MainActivity.kt
+++ app/src/main/java/com/example/rickmortyapi/MainActivity.kt
@@ -10,16 +10,41 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.network.Character
+import com.example.network.KtorClient
 import com.example.network.TestFile
 import com.example.rickmortyapi.ui.theme.RickMortyAPITheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
+
+    private val ktorClient = KtorClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            var character by remember {
+                mutableStateOf<Character?>(null)
+            }
+
+            LaunchedEffect(
+                key1 = Unit,
+                block = {
+                    delay(3000)
+                    character = ktorClient.getCharacer(12)
+                }
+            )
+
+
             RickMortyAPITheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
@@ -30,6 +55,8 @@ class MainActivity : ComponentActivity() {
                         )
 
                         TestFile()
+
+                        Text(text = character?.name ?: "No character")
                     }
                 }
             }
